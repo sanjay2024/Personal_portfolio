ba3b7507216fffdc940c5ec764d00133f79dddb4
sanjay <20euit128@skcet.ac.in>
2023-09-14 20:07:32 +0530

    liite changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import VMAIL from "../../Assets/VMAIL.jpg";
+import audioToSignLanguage from "../../Assets/audio_to_sign.jpg";
+import disney from "../../Assets/disney.jpg";
+import BookMania from "../../Assets/BookMania.jpg";
 
 function Projects() {
   return (
@@ -23,70 +21,54 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={disney}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="MovieTime"
+              description="Developed an online movie and TV show website, utilizing Reactjs, React Bootstrap, firebase and Redux for an better user experience.
+Implemented features such as browsing and streaming movie trailer and TV shows, user authentication and state management using redux, while leveraging Firebase for backend services
+"
+              ghLink="https://github.com/sanjay2024/Movie-Time"
+              demoLink="https://movie-time-new.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={BookMania}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="BookMania"
+              description="Developed a full-stack web application using Node.js/Express, React/Redux, and MongoDB/Mongoose for user management and authentication, including registration, login/logout, profile viewing, and updating
+Implemented secure authentication using JSON Web Tokens (JWT) and bcrypt for password hashing, while ensuring efficient database modeling and association for optimal data storage and retrieval.
+"
+              ghLink="https://github.com/sanjay2024/book-mania"
+              demoLink="https://bookmania12.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={audioToSignLanguage}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="audioToSignLanguage"
+              description="A web application which takes in live audio speech recording as input, converts it into text and displays the relevant Indian Sign Language animations."
+              ghLink="https://github.com/sanjay2024/Audio_To_SignLanguage_converter"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={VMAIL}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="VMAIL"
+              description="voice based email sender for blind persons
+              The project is a Python-based programme that uses speech to text voice 
+	 response for visually challenged people, allowing everyone to manage 	 their mail accounts using only their voice.  read, send, and complete 
+	 any other necessary chores.
+"
+              ghLink="https://github.com/sanjay2024/MiniProject-Voice--Based-Email-Sender"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
